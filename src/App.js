fe909b4ac52edc347e74fe7ee194393fa3d7ca6c
Richard Brooks <rickbrooks22@gmail.com>
2018-07-17 13:54:43 -0400

    created base shell for react exercise

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <button>CLICK ME!</button>
+      <div className="counter-box">
+        <div className="counter">3</div>
+        <div className="counter">1</div>
+        <div className="counter">1</div>
+      </div>
+    </div>
+
+  );
+};
+
+export default App;
