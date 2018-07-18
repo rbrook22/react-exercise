bcedeed09c069ff21f5604531ae6367a06aca651
Richard Brooks <rickbrooks22@gmail.com>
2018-07-18 10:50:56 -0400

    shifted counterValues to return method

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function converNumToCounter(value) {
   );
 }
 // NOt quite the react way
-let counterValues=[42, 3, 18, 29, 86, 2];
-let counterElements = counterValues.map(converNumToCounter);
+// let counterValues=[42, 3, 18, 29, 86, 2];
+// let counterElements = counterValues.map(converNumToCounter);
 
 class App extends React.Component {
   constructor (props) {
@@ -26,7 +26,7 @@ class App extends React.Component {
       <div className="container">
         <button onClick={handleClick}>CLICK ME!</button>
         <div className="counter-box">
-          {counterElements}
+          {this.state.counterValues.map(converNumToCounter)}
         </div>
       </div>
     );
