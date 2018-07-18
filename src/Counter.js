389282d162981daafdf6c84d56fbb14c160a802a
Richard Brooks <rickbrooks22@gmail.com>
2018-07-18 09:52:04 -0400

    added state props

--- src/Counter.js
+++ src/Counter.js
@@ -10,12 +10,20 @@ import React from 'react';
 
 // Rule Number One: Must extend Reac.Component
 class Counter extends React.Component {
+    // Rule Number 4: accept props in the constructor method
+    constructor(props) {
+        super(props);
+        // State is *my* stuff and always an object
+        this.state = {
+            currentValue: props.initialValue
+        };
+    }
     // Rule Number 2: Must have a Render Method
     render() {
         // Rule Number 3: Must return some JSX or a call to React.createElement
         return(
             <div className="counter">
-            3
+                {this.state.currentValue}
             </div>
         );
     }
